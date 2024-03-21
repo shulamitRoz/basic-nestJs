6626afb3c4fdc56dea51aa5024d25872f3dfddc6
shulamitRoz <shulamit6670@gmail.com>
2024-03-21 17:28:31 +0200

    update-db

--- src/data-base/database.module.ts
+++ src/data-base/database.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/cars'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/cars'),
   ],
 })
 export class DatabaseModule {}
